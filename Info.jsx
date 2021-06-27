c20ed44296bb2674bffd4769991cd0ab7a1c9709
Mihir2704 <86527391+Mihir2704@users.noreply.github.com>
2021-06-27 12:17:32 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1> Javascript and Rect.js </h1>
+      <p> Basic Web Dev Reatc Js Bootcamp </p>
+    </div>
+  );
+}
+export default Info;
---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom, { render } from "react-dom";
+import App from "./components/App";
+ReactDom.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
